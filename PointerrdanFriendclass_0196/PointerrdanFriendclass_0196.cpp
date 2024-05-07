41589c4ee7ece48324ed5372958021be9a803405
Wirayudhas <arturwirayuda@gmail.com>
2024-05-07 12:21:15 +0700

    menampilkan induk 3

--- PointerrdanFriendclass_0196/PointerrdanFriendclass_0196.cpp
+++ PointerrdanFriendclass_0196/PointerrdanFriendclass_0196.cpp
@@ -16,5 +16,10 @@ int main() {
 	mhs.showNim();     // Member access operator
 	mahasiswa& refMhs = mhs;    // pointer reference refMhs
 	refMhs.nim = 2;           // Mmember Access operator
+	mhs.showNim();
+	mahasiswa* pMhs = &mhs;  //point dereference pMhs
+	pMhs->nim = 3;                   //Arrow operator
+	pMhs->showNim();                // Arrow operator
+	return 0;
 
 }
